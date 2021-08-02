62778b2e53eea5edfe646e6bc7b57c8f935c0bfe
DavidRacz90 <davidracz90@gmail.com>
2021-08-02 20:29:17 +0200

    mocha timeouts

--- mocha/mocha2.js
+++ mocha/mocha2.js
@@ -1,25 +1,29 @@
-const assert = require('assert');
+const assert = require('assert')
 
-describe('Mathetmatical Operations - Test Suite', () => {
-  const a = 10;
-  const b = 10;
-  it.skip('Addition of two numbers', () => {
-    let sum = a + b;
-    assert.equal(sum, 20);
-  });
-  
+describe('Mathetmatical Operations - Test Suite', function() {
+  this.beforeEach(function(done){
+    this.timeout(500)
+    setTimeout(done, 3000)
+  })
+  const a = 10
+  const b = 10
+  it('Addition of two numbers', function(done) {
+    let sum = a + b
+    assert.equal(sum, 20)
+  })
+
   it('Subsctraction of two numbers', () => {
-    let sum = a -b;
-    assert.equal(sum, 0);
-  });
+    let sum = a - b
+    assert.equal(sum, 0)
+  })
 
   it('Multiple of two numbers', () => {
-    let sum = a * b;
-    assert.equal(sum, 100);
-  });
+    let sum = a * b
+    assert.equal(sum, 100)
+  })
 
   it('Divison of two numbers', () => {
-    let sum = a / b;
-    assert.equal(sum, 1);
-  });
-});
+    let sum = a / b
+    assert.equal(sum, 1)
+  })
+})
