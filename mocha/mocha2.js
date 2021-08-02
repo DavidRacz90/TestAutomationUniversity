b70eaa5d7e65d5aae9a6b507996bad5c4a4d56c1
DavidRacz90 <davidracz90@gmail.com>
2021-08-02 20:03:31 +0200

    mocha features

--- mocha/mocha2.js
+++ mocha/mocha2.js
@@ -1,30 +1,24 @@
 const assert = require('assert');
 
 describe('Mathetmatical Operations - Test Suite', () => {
-  it('Addition of two numbers', () => {
-    let a = 10;
-    let b = 10;
+  const a = 10;
+  const b = 10;
+  it.skip('Addition of two numbers', () => {
     let sum = a + b;
     assert.equal(sum, 20);
   });
   
   it('Subsctraction of two numbers', () => {
-    let a = 10;
-    let b = 10;
     let sum = a -b;
     assert.equal(sum, 0);
   });
 
   it('Multiple of two numbers', () => {
-    let a = 10;
-    let b = 10;
     let sum = a * b;
     assert.equal(sum, 100);
   });
 
   it('Divison of two numbers', () => {
-    let a = 10
-    let b = 10
     let sum = a / b;
     assert.equal(sum, 1);
   });
